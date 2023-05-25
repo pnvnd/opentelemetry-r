1560377c99f63eb888bb1c21e6c568b0beb76330
pnvnd <peter@datacrunch.ca>
2023-05-25 17:44:51 -0400

    chore: restructured directories

--- batch/jobs/job2.R
+++
@@ -1,5 +0,0 @@
-# Basic job to download CSV data from the web and save in some directory
-# data2 <- read.csv("batch/input/conposcovidloc_2021.csv", header=TRUE)
-data2 <- read.csv("https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/822d637d-3416-4ad8-b09b-a748ae1370e0/download/conposcovidloc_2021.csv", header=TRUE)
-dataB <- rbind(data2)
-write.csv(dataB, "batch/output/conposcovidlocB.csv")
--- plumber/plumberSrv.R
+++
@@ -1,7 +0,0 @@
-library(openmetrics)
-library(plumber)
-
-register_default_metrics()
-srv <- plumber::plumb("plumber.R")
-srv <- register_plumber_metrics(srv)
-srv$run(host="0.0.0.0", port=c(8000))
